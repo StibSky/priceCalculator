6a30c2596a7c7a6fa0bec66179745bb9be159e03
AnnKemp <ann.kemp@scarlet.be>
2020-02-17 11:32:35 +0100

    call data for groups

---
+++ Model/Groups.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+class Groups
+{
+
+    private $id;
+    private $name;
+    private $description;
+    private $price;
+
+    public function __construct(int $id, string $name, string $description, int $price)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+    }
+
+    public function getProductName() : string
+    {
+        return $this->name;
+    }
+
+    public function getProductId() : int
+    {
+        return $this -> id;
+    }
+
+    public function getProductDescription() : string {
+        return  $this -> description;
+    }
+    public function getProductPrice() : int {
+        return  $this -> price;
+    }
+}
