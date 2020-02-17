daa0faaeae72458dd71113763070685fb92524b7
AnnKemp <ann.kemp@scarlet.be>
2020-02-17 10:23:49 +0100

    adding new product en productmaker pages

---
+++ Model/ProductMaker.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+class ProductMaker
+{
+//get json
+    public function fetchProducts(): array
+    {
+        $list = [];
+        $json = json_decode(file_get_contents('Data/products.json'), true);
+        foreach ($json AS $productJson) {
+            $productlist[] = new ProductMaker($productJson['id'], $productJson['name'], $productJson['description'], $productJson['price']);
+        }
+        return $productlist;
+    }
+}
