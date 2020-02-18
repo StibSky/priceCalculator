810656989d3dba0e814756eb75df3f4c4d4b9f0b
AnnKemp <ann.kemp@scarlet.be>
2020-02-18 10:56:16 +0100

    added some bootstrap to the selects

--- View/homepage.php
+++ View/homepage.php
@@ -9,27 +9,33 @@
 </head>
 <body>
 <?php require 'includes/header.php' ?>
+<div class="form-group">
 <form action="#" method="post">
     <section>
+        <div class="form-group">
         <label for="users">Choose a person:</label>
 
-        <select id="users" name="users">
+        <select id="users" name="users" class="form-control">
 
             <?php for ($i = 0; $i < count($userArray); $i++): ?>
                 <option value= <?php echo $userArray[$i]->getId() ?>> <?php echo $userArray[$i]->getName() ?></option>
             <?php endfor; ?>
         </select>
+        </div>
     </section>
     <section>
+        <div class="form-group">
         <label for="product">Choose a product:</label>
-        <select name="product" id="product">
+        <select name="product" id="product" class="form-control">
             <?php for ($i = 0; $i < count($productArray); $i++): ?>
                 <option value= <?php echo $productArray[$i]->getProductId() ?>> <?php echo $productArray[$i]->getProductName() ?></option>
             <?php endfor; ?>
         </select>
+        </div>
     </section>
-    <input type="submit" value="require" name="submitButton">
+    <input type="submit" value="require" name="submitButton" class="btn btn-primary">
 </form>
+</div>
 <div class="card" style="width: 18rem;">
     <div class="card-body">
     <p>You are <?php echo $userArray[$userId]->getName() ?> and you have chosen: </p>
