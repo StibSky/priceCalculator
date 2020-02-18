2dca2dc2c57c8a9b615e3f1dede43650b8ea55ca
AnnKemp <ann.kemp@scarlet.be>
2020-02-18 15:58:37 +0100

    table altered

--- View/homepage.php
+++ View/homepage.php
@@ -68,24 +68,6 @@
         <td>Otto</td>
         <td>@mdo</td>
     </tr>
-    <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-    </tr>
-    <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
-    </tr>
     </tbody>
 </table>
 <?php require 'includes/footer.php' ?>
