f7ed8b331eb4699dbb73f1ec4594b998d6bf53a7
Becode <stevekeersma@gmail.com>
2020-02-18 09:51:10 +0100

    added group object array

--- Model/Groups.php
+++ Model/Groups.php
@@ -6,14 +6,16 @@ class Groups
 
     private $id;
     private $name;
-    private $discount;
+    private $variable_discount;
+    private $fixed_discount;
     private $group_id;
 
-    public function __construct(int $id, string $name, string $discount, int $group_id)
+    public function __construct(int $id, string $name, int $variable_discount, int $fixed_discount, int $group_id)
     {
         $this->id = $id;
         $this->name = $name;
-        $this->discount = $discount;
+        $this->varDiscount = $variable_discount;
+        $this->fixedDiscount = $fixed_discount;
         $this->group_id = $group_id;
     }
 
@@ -27,9 +29,6 @@ class Groups
         return $this -> id;
     }
 
-    public function getGroupDiscount() : string {
-        return  $this -> discount;
-    }
     public function getGroupGroupId() : int {
         return  $this -> group_id;
     }
