7efbf59a0a4f9909c805dbd3fcfdecbc5bec2e7c
Becode <stevekeersma@gmail.com>
2020-02-18 15:59:08 +0100

    finished all calculations

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -66,6 +66,8 @@ class HomepageController
         }
 
 
+
+
         // THIS LOOPS SHOULD WORK BUT DOESNT YET, CHECK BELLOW FOR HARDCODING
 
 
@@ -117,7 +119,6 @@ class HomepageController
 //       $customerGroupDisplay = getAllGroups($userArray[$userId]->getgroupId());
 
 
-
         //HARDCODING THE GROUPS OF A USER, SHOULD BE IN A LOOP
         $allUserGroups = [];
 
@@ -125,17 +126,90 @@ class HomepageController
         $userGroupId = $userArray[$userId]->getgroupId();
 
 
-
         array_push($allUserGroups, $groupArray[$userGroupId]);
         array_push($allUserGroups, $groupArray[$groupGroupId]);
         if ($groupArray[$groupGroupId]->getGroupGroupId() == 10000000) {
-            echo "stop";
+
         } else {
             array_push($allUserGroups, $groupArray[$groupArray[$groupGroupId]->getGroupGroupId()]);
         }
 
         var_dump($allUserGroups);
 
+        $variableArray = [];
+        $fixedArray = [];
+
+        for ($i = 0; $i < count($allUserGroups); $i++) {
+
+            if ($allUserGroups[$i]->getFixedDiscount() != 0) {
+                array_push($fixedArray, $allUserGroups[$i]->getFixedDiscount());
+            }
+            if ($allUserGroups[$i]->getVariableDiscount() != 0) {
+                array_push($variableArray, $allUserGroups[$i]->getVariableDiscount());
+            }
+
+        }
+
+        var_dump($fixedArray);
+        var_dump($variableArray);
+
+        if (!empty($variableArray)) {
+            $maxVariable = max($variableArray);
+        } else {
+            $maxVariable = 0;
+        }
+
+        function countFixed($v1, $v2) {
+            return $v1 + $v2;
+        }
+
+
+
+        if (!empty($fixedArray)) {
+            $countFixed = array_reduce($fixedArray, "countFixed");
+        } else {
+            $countFixed = 0;
+        }
+
+        echo $maxVariable;
+        echo "<br>". $countFixed;
+
+
+        $fixedreductionResult = round($productArray[$productId]->getProductPrice() - $countFixed,2);
+        if ($fixedreductionResult < 0) {
+            $fixedreductionResult =0;
+        }
+
+        echo "<br>result - fixed " . $fixedreductionResult;
+
+        $variablePercentage = $maxVariable /100;
+
+        $reductionVariable = $variablePercentage *$productArray[$productId]->getProductPrice();
+
+        $variableReductionResult = round($productArray[$productId]->getProductPrice() - $reductionVariable,2);
+
+        echo "<br>result - variable " . $variableReductionResult;
+
+        $compareArray = [];
+        array_push($compareArray,$fixedreductionResult,$variableReductionResult );
+        $winningDiscount = min($compareArray);
+
+        if ($winningDiscount == $fixedreductionResult ) {
+            echo "<br>fixed wins";
+        } elseif ($winningDiscount == $variableReductionResult ) {
+            echo "<br>variable wins";
+        }
+
+
+        $finalResultMinFixed = $productArray[$productId]->getProductPrice()- $countFixed;
+        $finalResultPercentage = $finalResultMinFixed * $variablePercentage;
+        $finalResult = round($finalResultMinFixed - $finalResultPercentage , 2);
+
+        if ($finalResult < 0) {
+            $finalResult = 0;
+        }
+
+        echo "<br> final result ".$finalResult;
 
         //you should not echo anything inside your controller - only assign vars here
         // then the view will actually display them.    echo($everyone[0]);
