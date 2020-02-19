b1771e41612e36397cad832d57cb620b9590a1be
AnnKemp <ann.kemp@scarlet.be>
2020-02-19 15:15:49 +0100

    @ added

--- View/includes/footer.php
+++ View/includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="text-white bg-secondary fixed-bottom p-0 m-0">
-<p class="text-center mt-3">Made by Steve & Ann &copy; BeCode</p>
+<p class="text-center mt-3">Made by Steve & Ann @ BeCode</p>
 </footer>
 </body>
 </html>
