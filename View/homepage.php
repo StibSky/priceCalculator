c16589a1ef213c361954b0e689ae6270d4c1308a
AnnKemp <ann.kemp@scarlet.be>
2020-02-19 12:13:29 +0100

    version with seperate login

--- View/homepage.php
+++ View/homepage.php
@@ -74,8 +74,8 @@
         <td><?php echo $countFixed ?></td>
         <td><?php echo $maxVariable."%" ?></td>
         <td><?php echo $finalResult ?></td>
-        <td><?php echo ""; ?></td> <?echo // moet nog gemaakt worden op de homecontrollerpage ?>
-        <td><?php echo ""; ?></td> <? echo // moet nog gemaakt worden op de homecontrollerpage ?>
+        <td><?php echo ""; ?></td> <?echo "";// moet nog gemaakt worden op de homecontrollerpage ?>
+        <td><?php echo ""; ?></td> <? echo "";// moet nog gemaakt worden op de homecontrollerpage ?>
     </tr>
     </tbody>
 </table>
