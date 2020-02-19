bc625240a8cfe573be89bb454baab005cbfe2d14
AnnKemp <ann.kemp@scarlet.be>
2020-02-19 10:42:57 +0100

    lay-out footer and header adjusted also the card fixed

--- View/includes/footer.php
+++ View/includes/footer.php
@@ -1,5 +1,5 @@
-<footer class="ml-5">
-<p class="text-center">Made by Steve & Ann &copy; BeCode</p>
+<footer class="text-white bg-secondary fixed-bottom p-0 m-0">
+<p class="text-center mt-3">Made by Steve & Ann &copy; BeCode</p>
 </footer>
 </body>
 </html>
