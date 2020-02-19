d05687c8b35ef3b07afb2a6fbcc63464fe03a06c
Becode <stevekeersma@gmail.com>
2020-02-19 14:03:26 +0100

    calculator class now has functinality

--- Model/Calculator.php
+++ Model/Calculator.php
@@ -3,22 +3,29 @@ declare(strict_types=1);
 
 class Calculator
 {
-    private $fixedArray =[];
-    private $variableArray =[];
+    private $fixedArray = [];
+    private $variableArray = [];
 
-    public function makeDiscountArrays($array)
+    public function makeFixed($array)
     {
-
         for ($i = 0; $i < count($array); $i++) {
             if ($array[$i]->getFixedDiscount() != 0) {
                 array_push($this->fixedArray, $array[$i]->getFixedDiscount());
-                return $this->fixedArray;
-            }
-            if ($array[$i]->getVariableDiscount() != 0) {
-                array_push($this->variableArray, $array[$i]->getVariableDiscount());
-                return $this->variableArray;
             }
 
         }
+        return $this->fixedArray;
+    }
+
+
+    public function makeVariable($array)
+    {
+        for ($i = 0; $i < count($array); $i++) {
+            if ($array[$i]->getVariableDiscount() != 0) {
+                array_push($this->variableArray, $array[$i]->getVariableDiscount());
+
+            }
+        }
+        return $this->variableArray;
     }
 }
